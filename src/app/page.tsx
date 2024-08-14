b0e88c9ac7b5b3a8ff62e972639268ee61036746
Yousef Saeed <137455394+YousefSaeed668@users.noreply.github.com>
2024-08-14 04:35:47 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
+"use client";
 import { Features } from "@/components/Features";
 import { FeaturesDetails } from "@/components/FeaturesDetails";
 import { Hero } from "@/components/Hero";
 import { HowItWorks } from "@/components/HowItWorks";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setIsLoading(false);
+    };
+
+    if (document.readyState === "complete") {
+      handleLoad();
+    } else {
+      window.addEventListener("load", handleLoad);
+    }
+
+    return () => window.removeEventListener("load", handleLoad);
+  }, []);
+  if (isLoading) {
+    return "true";
+  }
   return (
     <div>
       <Hero />
